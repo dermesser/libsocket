5357bdb0c4bf6e9ed540f7b43d0742a5d5fae3eb
lbo <lbo@spheniscida.de>
2012-02-24 15:49:52 +0100

    Fixed wrong comma in header file

--- headers/libinetsocket.h
+++ headers/libinetsocket.h
@@ -38,9 +38,9 @@
 
 // Creates socket, connects it and gives it back
 //                Hostname          Port/Service         Transport protocol (TCP or UDP)  Network Protocol (IPv4 or IPv6)  may be SOCK_NONBLOCK (socket won't block) and SOCK_CLOEXEC (closes socket on exec() call)
-extern int create_isocket(const char* host, const char* service, char proto_osi4,         char proto_osi3,                 
+extern int create_isocket(const char* host, const char* service, char proto_osi4,         char proto_osi3                 
 # ifdef __linux__
-		int flags);
+		, int flags);
 # else
 );
 # endif
