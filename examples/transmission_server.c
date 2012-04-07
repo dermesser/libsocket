434813c85ca27aec9ed22e3b079679f8051afa87
lbo <lbo@spheniscida.de>
2012-04-07 21:59:10 +0200

    Updated examples/transmission_server.c

--- examples/transmission_server.c
+++ examples/transmission_server.c
@@ -1,20 +1,20 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include "headers/libinetsocket.h"
+# include "../headers/libinetsocket.h"
 
 int main(void)
 {
-	int cfd, sfd = create_issocket("::","55555",TCP,IPv6);
+	int cfd, sfd = create_inet_server_socket("::","55555",TCP,IPv6);
 	char* buf = calloc(16,1);
 
-	cfd = accept_issocket(sfd,0,0,0,0,0);
+	cfd = accept_inet_stream_socket(sfd,0,0,0,0,0);
 
 	read(cfd,buf,15);
 
 	printf("%s\n",buf);
 
-	destroy_isocket(sfd);
+	destroy_inet_socket(sfd);
 
 	return 0;
 }
