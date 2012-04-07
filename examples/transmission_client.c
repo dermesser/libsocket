647ba410bfe524d1adab3c1e3a089f5eaca84d09
lbo <lbo@spheniscida.de>
2012-04-07 21:56:06 +0200

    Updated examples/transmission_client.c

--- examples/transmission_client.c
+++ examples/transmission_client.c
@@ -1,16 +1,16 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include "headers/libinetsocket.h"
+# include "../headers/libinetsocket.h"
 
 int main(void)
 {
-	int sfd = create_isocket("2001:554:2323:aabc::22ed","55555",TCP,IPv6,0);
+	int sfd = create_inet_stream_socket("2001:554:2323:aabc::22ed","55555",IPv6,0);
 	char* buf = "abcde";
 
 	write(sfd,buf,5);
 
-	destroy_isocket(sfd);
+	destroy_inet_socket(sfd);
 
 	return 0;
 }
