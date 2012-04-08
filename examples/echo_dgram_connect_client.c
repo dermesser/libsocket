471162914027cba464b2f8e7f34fc5917e7da309
lbo <lbo@spheniscida.de>
2012-04-08 19:16:17 +0200

    Added example with connected dgram sockets

---
+++ examples/echo_dgram_connect_client.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "../headers/libinetsocket.h"
+# include <unistd.h>
+# include <string.h>
+
+/*
+ * This example is part of libsocket/libinetsocket
+ * 
+*/
+
+int main(void)
+{
+	int sfd;
+	char buf[16];
+
+	memset(buf,0,16);
+
+	sfd = create_inet_dgram_socket(IPv4,0);
+	
+	connect_inet_dgram_socket(sfd,"localhost","1234");
+	
+	write(sfd,"abcde",5);
+	
+	sleep(2);
+	
+	read(sfd,buf,5);
+
+	write(1,buf,5);
+
+	destroy_inet_socket(sfd);
+
+	return 0;
+}
