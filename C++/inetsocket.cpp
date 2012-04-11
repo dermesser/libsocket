44196b06f4ba2027f94ba14238f33a62c2fcc8ce
lbo <lbo@spheniscida.de>
2012-04-11 18:31:04 +0200

    Various changes II - some dgram stuff

--- C++/inetsocket.cpp
+++ C++/inetsocket.cpp
@@ -62,12 +62,12 @@ namespace libsocket
 		public:
 
 		inet_stream(void);
-		inet_stream(const char* host, const char* port, int proto_osi3, int flags=0);
-		
+		inet_stream(const char* host, const char* port, int proto_osi3, int flags=0); // flags: socket()
+
 		~inet_stream();
 
 		// Real actions
-		void connect(const char* host, const char* port, int proto_osi3, int flags=0);
+		void connect(const char* host, const char* port, int proto_osi3, int flags=0); // flags: socket()
 		void shutdown(int method);
 		void try_to_destroy(void);
 		void destroy(void);
@@ -77,12 +77,12 @@ namespace libsocket
 		friend inet_stream& operator<<(inet_stream& sock, const char* str);
 		friend inet_stream& operator<<(inet_stream& sock, string& str);
 
-		ssize_t snd(const void* buf, size_t len, int flags=0);
+		ssize_t snd(const void* buf, size_t len, int flags=0); // flags: send()
 
 		// I
 		friend inet_stream& operator>>(inet_stream& sock, string& dest);
 
-		ssize_t rcv(void* buf, size_t len, int flags=0);
+		ssize_t rcv(void* buf, size_t len, int flags=0); // flags: recv()
 
 		// Getters
 		int getfd(void) const;
@@ -138,14 +138,17 @@ namespace libsocket
 
 	void inet_stream::try_to_destroy(void)
 	{
-		close(sfd);
-		sfd = -1;
+		if ( sfd != -1 )
+		{
+			close(sfd);
+			sfd = -1;
+		}
 	}
 
 	void inet_stream::destroy(void)
 	{
 		if ( -1 == sfd )
-			throw inet_exception(__FILE__,__LINE__,"Socket was not connected!\n");
+			return;
 
 		if ( 0 > destroy_inet_socket(sfd) )
 			throw inet_exception(__FILE__,__LINE__,"Could not close socket\n");
@@ -265,33 +268,89 @@ namespace libsocket
 		bool connected;
 
 		public:
-		
-		inet_dgram(int proto_osi3,int flags=0); // socket()
-		inet_dgram(const char* host, const char* port, int proto_osi3, int flags=0); // socket() connect()
-		
+
+		// Only create socket
+		inet_dgram(int proto_osi3,int flags=0); // Flags: socket()
+		// Create socket and connect it
+		inet_dgram(const char* host, const char* port, int proto_osi3, int flags=0); // Flags: socket()
+
 		~inet_dgram();
 
 		// actions
+		// connect/reconnect
 		void connect(const char* host, const char* port);
 		void deconnect(void);
 
 		// I/O
-
+		// destroy but don't complain
 		void try_to_destroy(void);
 		void destroy(void);
 
 		// I/O
 		// O
+		// only if connected
 		friend inet_stream& operator<<(inet_stream& sock, const char* str);
 		friend inet_stream& operator<<(inet_stream& sock, string& str);
 
-		ssize_t snd(const void* buf, size_t len, int flags);
-		ssize_t sndto(const void* buf, size_t len, const char* host, const char* port, int sndto_flags);
+		ssize_t snd(const void* buf, size_t len, int flaags=0);
+		ssize_t sndto(const void* buf, size_t len, const char* host, const char* port, int sndto_flags=0);
 		// I
 		friend inet_stream& operator>>(inet_stream& sock, string& dest);
 
-		ssize_t rcv(void* buf, size_t len, int flags);
-		ssize_t rcvfrom(void* buf, size_t len, char* host, size_t hostlen, char* port, size_t portlen, int rcvfrom_flags);
-
+		ssize_t rcv(void* buf, size_t len, int flags=0);
+		ssize_t rcvfrom(void* buf, size_t len, char* host, size_t hostlen, char* port, size_t portlen, int rcvfrom_flags=0);
 	};
+
+	inet_dgram::inet_dgram(int proto_osi3, int flags) : connected(false)
+	{
+		if ( -1 == (sfd = create_inet_dgram_socket(proto_osi3,flags)) )
+			throw inet_exception(__FILE__,__LINE__,"Could not create inet dgram socket!\n");
+
+		proto = proto_osi3;
+	}
+
+	inet_dgram::inet_dgram(const char* host, const char* port, int proto_osi3, int flags)
+	{
+		if ( -1 == (sfd = create_inet_dgram_socket(proto_osi3,flags)) )
+			throw inet_exception(__FILE__,__LINE__,"Could not create inet dgram socket!\n");
+
+		try {
+			connect(host,port);
+		} catch (inet_exception exc)
+		{
+			throw inet_exception(__FILE__,__LINE__,"Could not connect dgram socket\n");
+		}
+	}
+
+	inet_dgram::~inet_dgram(void)
+	{
+		try_to_destroy();
+	}
+
+	void inet_dgram::connect(const char* host, const char* port)
+	{
+		if ( -1 == (connect_inet_dgram_socket(sfd,host,port)) )
+			throw inet_exception(__FILE__,__LINE__,"Could not connect dgram socket!\n");
+
+		remote_host = host;
+		remote_port = port;
+	}
+
+	void inet_dgram::try_to_destroy(void)
+	{
+		if ( sfd != -1 )
+		{
+			close(sfd);
+			sfd = -1;
+		}
+	}
+
+	void inet_dgram::destroy(void)
+	{
+		if ( -1 == sfd )
+			return;
+
+		if ( -1 == destroy_inet_socket(sfd) )
+			throw inet_exception(__FILE__,__LINE__,"Could not close socket!\n");
+	}
 }
--- headers++/inetsocket.hpp
+++ headers++/inetsocket.hpp
@@ -53,7 +53,7 @@ namespace libsocket
 
 		inet_stream(void);
 		inet_stream(const char* host, const char* port, int proto_osi3, int flags=0);
-		
+
 		~inet_stream();
 
 		// Real actions
@@ -78,6 +78,45 @@ namespace libsocket
 		int getfd(void) const;
 		const char* gethost(void) const;
 		const char* getport(void) const;
+	};
+
+	class inet_dgram : public inet_socket
+	{
+		private:
+		bool connected;
+
+		public:
+
+		// Only create socket
+		inet_dgram(int proto_osi3,int flags=0); // Flags: socket()
+		// Create socket and connect it
+		inet_dgram(const char* host, const char* port, int proto_osi3, int flags=0); // Flags: socket()
+
+		~inet_dgram();
+
+		// actions
+		// connect/reconnect
+		void connect(const char* host, const char* port);
+		void deconnect(void);
+
+		// I/O
+		// destroy but don't complain
+		void try_to_destroy(void);
+		void destroy(void);
+
+		// I/O
+		// O
+		// only if connected
+		friend inet_stream& operator<<(inet_stream& sock, const char* str);
+		friend inet_stream& operator<<(inet_stream& sock, string& str);
+
+		ssize_t snd(const void* buf, size_t len, int flaags=0);
+		ssize_t sndto(const void* buf, size_t len, const char* host, const char* port, int sndto_flags=0);
+		// I
+		friend inet_stream& operator>>(inet_stream& sock, string& dest);
+
+		ssize_t rcv(void* buf, size_t len, int flags=0);
+		ssize_t rcvfrom(void* buf, size_t len, char* host, size_t hostlen, char* port, size_t portlen, int rcvfrom_flags=0);
 	};
 }
 # endif
