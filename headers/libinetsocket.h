416f18d6ac56ee0bacc1288055a0b84f7934b303
lbo <lbo@spheniscida.de>
2012-04-11 16:53:53 +0200

    Removed nonsense linux specific functions

--- headers/libinetsocket.h
+++ headers/libinetsocket.h
@@ -40,33 +40,14 @@
 
 # define NUMERIC 1
 
-extern int create_inet_stream_socket(const char* host, const char* service, char proto_osi3
-# ifdef __linux__
-		, int flags);
-# else
-);
-# endif
-
-extern int create_inet_dgram_socket(char proto_osi3
-# ifdef __linux__
-		, int flags);
-# else
-);
-# endif
-
+extern int create_inet_stream_socket(const char* host, const char* service, char proto_osi3, int flags);
+extern int create_inet_dgram_socket(char proto_osi3, int flags);
 extern ssize_t sendto_inet_dgram_socket(int sfd,void* buf, size_t size,char* host, char* service, int sendto_flags);
 extern ssize_t recvfrom_inet_dgram_socket(int sfd, void* buffer, size_t size, char* src_host, size_t src_host_len, char* src_service, size_t src_service_len, int recvfrom_flags, int numeric);
 extern int connect_inet_dgram_socket(int sfd, char* host, char* service);
 extern int destroy_inet_socket(int sfd);
 extern int shutdown_inet_stream_socket(int sfd, int method);
-
-extern int create_inet_server_socket(const char* bind_addr, const char* bind_port, char proto_osi4, char proto_osi3
-# ifdef __linux__
-		, int flags);
-# else
-);
-# endif
-
+extern int create_inet_server_socket(const char* bind_addr, const char* bind_port, char proto_osi4, char proto_osi3, int flags);
 extern int accept_inet_stream_socket(int sfd, char* src_host, size_t src_host_len, char* src_service, size_t src_service_len, int flags);
 
 # endif
