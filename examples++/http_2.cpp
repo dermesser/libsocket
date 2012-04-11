7356f21aa5823d3b5f866c036c43e46c0b1bba21
lbo <lbo@spheniscida.de>
2012-04-11 17:58:52 +0200

    Ported to default arguments

--- examples++/http_2.cpp
+++ examples++/http_2.cpp
@@ -11,13 +11,13 @@ int main(void)
 
 	char* buf = new char[10000];
 
-	libsocket::inet_stream sock(host.c_str(),port.c_str(),IPv4,0);
+	libsocket::inet_stream sock(host.c_str(),port.c_str(),IPv4);
 	
-	sock.snd("GET / HTTP/1.0\n\n",16,0);
+	sock.snd("GET / HTTP/1.0\n\n",16);
 
 	sock.shutdown(WRITE);
 
-	while ( 0 != (len=sock.rcv(buf,10000,0)) )
+	while ( 0 != (len=sock.rcv(buf,10000)) )
 		std::cout << string(buf,len); // Write only as many characters as we read
 
 	return 0;
