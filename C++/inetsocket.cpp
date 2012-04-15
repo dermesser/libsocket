5639edb9f890f7c3222331c33816fce31d48d2b4
lbo <lbo@spheniscida.de>
2012-04-15 09:17:49 +0200

    Restructured c++ source files

--- C++/inetsocket.cpp
+++ C++/inetsocket.cpp
@@ -3,6 +3,7 @@
 # include <string.h>
 # include "../headers/libinetsocket.h"
 # include "../headers++/socket.hpp"
+# include "../headers++/inetbase.hpp"
 # include <unistd.h>
 # include <sys/socket.h>
 # include <sys/types.h>
@@ -13,45 +14,6 @@
 namespace libsocket
 {
 	using std::string;
-
-	struct inet_exception
-	{
-		string mesg;
-
-		inet_exception(string,int,string);
-	};
-
-	inet_exception::inet_exception(string f, int l, string m)
-	{
-		char line[5];
-
-		line[4] = 0;
-
-		sprintf(line,"%i",l);
-
-		m.insert(0,": ");
-		m.insert(0,line);
-		m.insert(0,":");
-		m.insert(0,f);
-
-		mesg = m;
-	}
-
-/***************** inet_socket base class **********************/
-
-	class inet_socket : public socket
-	{
-		protected:
-		int proto;
-		string host;
-		string port;
-
-		public:
-
-		inet_socket();
-	};
-
-	inet_socket::inet_socket() : host(""), port("") {}
 
 /**************** inet_stream class (TCP inet sockets) *********/
 
