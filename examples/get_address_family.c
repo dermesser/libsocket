b8e327f63a1c0e0007bed5f98cb6df3d184530b9
lbo <lbo@spheniscida.de>
2012-10-21 16:30:27 +0200

    Added get_address_family function to libinetsocket

---
+++ examples/get_address_family.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "../headers/libinetsocket.h"
+# include <unistd.h>
+# include <string.h>
+
+/*
+ * This program shows which address family is available
+ * for a certain host
+ *
+ * Usage: $ get_address_family hostname.domain
+ *
+*/
+
+int main(int argc, char** argv)
+{
+
+	int af = get_address_family(argv[1]);
+
+	switch ( af )
+	{
+		case IPv4: 
+			printf("Supports IPv4\n");
+			break;
+		case IPv6:
+			printf("Supports IPv6\n");
+			break;
+		default:
+			printf("No valid result\n");
+	}
+
+	return 0;
+}
+
--- headers/libinetsocket.h
+++ headers/libinetsocket.h
@@ -55,6 +55,7 @@ extern int destroy_inet_socket(int sfd);
 extern int shutdown_inet_stream_socket(int sfd, int method);
 extern int create_inet_server_socket(const char* bind_addr, const char* bind_port, char proto_osi4, char proto_osi3, int flags);
 extern int accept_inet_stream_socket(int sfd, char* src_host, size_t src_host_len, char* src_service, size_t src_service_len, int flags,int accept_flags);
+extern int get_address_family(const char* hostname);
 
 # ifdef __cplusplus
 # ifdef _MIXED
