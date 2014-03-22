92a796745efbcc5594d6bf3e1ab21261762f6956
lbo <lbo@spheniscida.de>
2014-03-22 20:22:28 +0100

    Multicast listener example.

---
+++ examples/multicast-listen.c
@@ -0,0 +1,33 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <libsocket/libinetsocket.h>
+# include <netinet/in.h>
+# include <sys/socket.h>
+# include <unistd.h>
+# include <string.h>
+
+int main(int argc, char** argv)
+{
+
+    int c = 0;
+    char* buffer = malloc(8192);
+
+    //int sfd = create_multicast_socket("225.1.2.3","1230","eth0");
+    int sfd = create_multicast_socket("239.255.255.250","1900","wlan0"); // This group/port is used by UPnP devices. Maybe you see something?
+    //int sfd = create_multicast_socket("ff00::abcd","1900","eth0");
+
+    ssize_t received = 0;
+
+    if ( 0 > setsockopt(sfd,IPPROTO_IP,IP_MULTICAST_LOOP,&c,4) )
+    {
+        fprintf(stderr,"setsockopt failed.");
+        exit(1);
+    }
+
+    while ( 0 <= (received = read(sfd,buffer,8191)) )
+    {
+        write(0,buffer,received);
+    }
+
+    return 0;
+}
