7dacd45fc75f3e8ab31a30cc192048b6643ad760
Jett <whackashoe@gmail.com>
2015-09-21 00:39:43 -0500

    allow const std::string to be sent to unix stream and dgram clients

--- headers/streamclient.hpp
+++ headers/streamclient.hpp
@@ -57,7 +57,7 @@ namespace libsocket
 	    ssize_t rcv(void* buf, size_t len, int flags=0); // flags: recv()
 
 	    friend stream_client_socket& operator<<(stream_client_socket& sock, const char* str);
-	    friend stream_client_socket& operator<<(stream_client_socket& sock, string& str);
+	    friend stream_client_socket& operator<<(stream_client_socket& sock, const string& str);
 	    friend stream_client_socket& operator>>(stream_client_socket& sock, string& dest);
 
 	    void shutdown(int method=LIBSOCKET_WRITE);
