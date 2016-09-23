071a89f18afe14056d963b75f82a170a7e7a9914
Lewin Bormann <lewin@lewin-bormann.info>
2016-09-23 12:11:06 +0200

    Update documentation

--- doc/doxygen/html/search/functions_4.js
+++ doc/doxygen/html/search/functions_4.js
@@ -4,6 +4,6 @@ var searchData=
   ['get_5fpath',['get_path',['../classlibsocket_1_1unix__socket.html#a45c88937a7f5a211340bd580bc60ceeb',1,'libsocket::unix_socket']]],
   ['getconn',['getconn',['../classlibsocket_1_1dgram__client__socket.html#a0fc044c5f9a1aca4ced69f48ce0a84d3',1,'libsocket::dgram_client_socket']]],
   ['getfd',['getfd',['../classlibsocket_1_1socket.html#a29acfab57fbfad029ac686753335d6fd',1,'libsocket::socket']]],
-  ['gethost',['gethost',['../classlibsocket_1_1inet__socket.html#a2ea69ba4f08398406bafaa6786dabeb3',1,'libsocket::inet_socket']]],
-  ['getport',['getport',['../classlibsocket_1_1inet__socket.html#aa62a34024932ad45740269616351e54f',1,'libsocket::inet_socket']]]
+  ['gethost',['gethost',['../classlibsocket_1_1inet__socket.html#a9c34af31d554674779ebc8590e1a47b1',1,'libsocket::inet_socket']]],
+  ['getport',['getport',['../classlibsocket_1_1inet__socket.html#a43ad704ea93ce3b100529f9153c0e2ff',1,'libsocket::inet_socket']]]
 ];
